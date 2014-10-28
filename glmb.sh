139ddc92d6a22ed7cc694062805d85d06a2533d2
Eric Squires <eric.squires@gmail.com>
2014-10-28 07:21:59 -0400

    glmb - error handling when merge base is 1st commit
    also removed total line logic. The ^merge_base (don't include anything
    in the history involving merge_base) already handled that.

--- glmb.sh
+++ glmb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# PURPOSE 
+# PURPOSE
 #
-#   while git log commit1...commit2 does the same thing, it does not implement --graph 
+#   while git log commit1...commit2 does the same thing, it does not implement --graph
 #
-# USAGE 
+# USAGE
 #
 #   usage: glmb [git log options] [--format=0] sha1 sha2 [sha3 ...]
 #
@@ -12,30 +12,30 @@
 #
 
 #check for appropriate number of arguments
-if [[ $# -le 1 ]]; then 
+if [[ $# -le 1 ]]; then
     echo "usage: glmb [git log options] [--format=1] sha1 sha2 [sha3 ...]" >&2
     exit 1
 fi
 
 #process command line arguments
-if [[ $1 == '-h' || $1 == '--help' ]]; then 
-    echo "usage: glmb [git log options] [--format=1] sha1 sha2 [sha3 ...]" 
+if [[ $1 == '-h' || $1 == '--help' ]]; then
+    echo "usage: glmb [git log options] [--format=1] sha1 sha2 [sha3 ...]"
     exit 0
 fi
 
 format_output=1
 finished=false
 
-while [[ -n $1 ]]; do 
+while [[ -n $1 ]]; do
 
     tmp=$1
 
-    if [ ${tmp:0:9} == '--format=' ]; then 
+    if [ ${tmp:0:9} == '--format=' ]; then
         format_output=${tmp:9:1}
-    elif [ ${tmp:0:1} == '-' ]; then 
+    elif [ ${tmp:0:1} == '-' ]; then
         finished=true
         usr_options="$usr_options $1"
-    else 
+    else
         branches="$branches $1"
     fi
 
@@ -43,15 +43,28 @@ while [[ -n $1 ]]; do
 
 done
 
-merge_base="$(git merge-base $branches )"
-for b in $branches; do 
-    lines_bw_commits=$(git rev-list --count --left-right $b...$merge_base | cut -f 1 )
-    tot_lines_needed=$(( tot_lines_needed + lines_bw_commits ))
-done
-tot_lines_needed=$(( tot_lines_needed+1 ))
+#get the merge base.
+merge_base="$(git merge-base $branches)"
 
-if (( format_output == 1 )); then 
-    git log --pretty=format:'%C(yellow)%h %ad %Creset%s %C(red)%d %Cgreen[%cn] %Creset' --decorate --date=short --graph -$tot_lines_needed $usr_options $branches ^$merge_base~
+if git cat-file -t $merge_base~ > /dev/null 2>&1; then
+
+    #^ - exclude
+    #~ - parent
+    #^$merge_base~ - exclude everything before the merge base
+    mb_arg="^$merge_base~"
+
 else
-    git log -$tot_lines_needed $usr_options $branches ^$merge_base~
+
+    #if the parent of the merge base does not exist, then one of the input
+    #sha's is the first commit so there is no need to bother with a merge base
+    #see here:
+    #http://stackoverflow.com/questions/18515488/how-to-check-if-the-commit-exists-in-a-git-repository-by-its-sha-1
+    mb_arg=""
+
+fi
+
+if (( format_output == 1 )); then
+    git log --pretty=format:'%C(yellow)%h %ad %Creset%s %C(red)%d %Cgreen[%cn] %Creset' --decorate --date=short --graph $usr_options $branches $mb_arg
+else
+    git log -$tot_lines_needed $usr_options $branches $mb_arg
 fi
