4af802eeabe0173a240df183c86b278e1a3e3a85
Eric Squires <eric.g.squires@gmail.com>
2015-09-27 21:29:29 -0400

    awesome - remove float layout, add tiling

--- gen_config.lua
+++ gen_config.lua
@@ -55,12 +55,12 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.fair,
+    awful.layout.suit.tile.left,
     awful.layout.suit.magnifier,
     awful.layout.suit.max,
-    awful.layout.suit.floating,
+    --awful.layout.suit.floating,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.spiral,
