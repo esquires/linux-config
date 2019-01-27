c52aa6d9946aba28aad5976b2399e3b4acb50824
Eric Squires <eric.g.squires@gmail.com>
2019-01-26 21:40:25 -0500

    fix cmd_monitor to use notify-send with sound notification

--- scripts/cmd_monitor.py
+++ scripts/cmd_monitor.py
@@ -1,31 +1,73 @@
+#!/usr/bin/env python
 """Run a command and give auditory feedback."""
 import argparse
-import subprocess
-import time
-from espeak import espeak
+import subprocess as sp
+
+
+def is_make_cmd(cmd):
+    return cmd.strip(' ').split(' ')[0] == 'make'
+
+
+def has_make_output(stdout):
+    return any((line for line in stdout
+                if 'Building' in line or 'Linking' in line))
 
 
 def main():
     """Pass command line argument to subprocess."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("cmd", nargs='+')
+    parser.add_argument(
+        '-i', '--ignore-make-output', action='store_true',
+        help='whether to ignore make outputs that do not build anything')
+    parser.add_argument(
+        '-t', '--timeout', type=float, default=2,
+        help='how long in seconds the notification should last')
+    parser.add_argument(
+        "-C", "--command_dir", default=".",
+        help='directory to run command from')
+    parser.add_argument("cmd")
     args = parser.parse_args()
 
-    try:
-        words = [args.cmd[0]]
-    except TypeError:
-        words = []
+    print('calling "{}"'.format(args.cmd))
 
-    try:
-        subprocess.check_call(args.cmd)
-        words += ["successful."]
-    except subprocess.CalledProcessError:
-        words += ["failed."]
+    sound_dir = '/usr/share/sounds/ubuntu/stereo/'
+    # process = sp.Popen(args.cmd.split(' '), cwd=args.command_dir,
+    #                    stdout=sp.PIPE, stderr=sp.PIPE)
 
-    espeak.synth(" ".join(words))
+    process = sp.Popen(
+        args.cmd.split(' '), cwd=args.command_dir, stdout=sp.PIPE, bufsize=1)
+    stdout = []
+    for line in iter(process.stdout.readline, b''):
+        line_decoded = line.decode('utf-8').strip('\n')
+        print(line_decoded)
+        stdout.append(line_decoded)
+    process.stdout.close()
+    process.wait()
 
-    # without this the words get cutoff
-    time.sleep(2)
+    if (args.ignore_make_output and
+            is_make_cmd(args.cmd) and
+            not has_make_output(stdout)):
+        print(('not notifying because it is a '
+               'make command without anything built'))
+        return
+
+    if process.returncode == 0:
+        result = "succeeded"
+        urgency = 'normal'
+        sound = sound_dir + 'button-toggle-on.ogg'
+    else:
+        result = "failed"
+        urgency = 'critical'
+        sound = sound_dir + 'button-pressed.ogg'
+
+    title = 'cmd_monitor'
+    sp.Popen(
+        ['notify-send',
+         '-t', str(args.timeout * 1000),
+         '-u', urgency,
+         title,
+         '"{}" {}'.format(args.cmd, result)])
+    sp.Popen(['paplay', sound])
 
 
 if __name__ == '__main__':
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -21,6 +21,7 @@ sudo apt install -y \
     libnotify-dev \
     ninja-build \
     flake8 \
+    notify-osd \
     flawfinder
 
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
@@ -31,9 +32,9 @@ CONFIG_DIR="/home/$USER/repos/linux-config"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 mkdir ~/bin
-ln -v -s $CONFIG_DIR/bin/glmb.sh /home/$USER/bin/glmb
-ln -v -s $CONFIG_DIR/bin/cpp_static_wrapper.py /home/$USER/bin
-ln -v -s $CONFIG_DIR/bin/cmd_monitor.py /home/$USER/bin/cmd_monitor
+ln -v -s $CONFIG_DIR/scripts/glmb.sh /home/$USER/bin/glmb
+ln -v -s $CONFIG_DIR/scripts/cpp_static_wrapper.py /home/$USER/bin
+ln -v -s $CONFIG_DIR/scripts/cmd_monitor.py /home/$USER/bin/cmd_monitor
 
 mkdir ~/.vim
 mkdir ~/.vim/{bundle,autoload,swaps,backups}
