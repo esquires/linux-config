d370c3493a85ad64df2eb7e9e799e351407f5759
Eric Squires <eric.squires@gtri.gatech.edu>
2020-06-07 04:59:52 -0400

    don't use magnifier in awesome

--- rc4.lua
+++ rc4.lua
@@ -65,7 +65,7 @@ awful.layout.layouts = {
         awful.layout.suit.tile.left,
         awful.layout.suit.fair,
         awful.layout.suit.max,
-        awful.layout.suit.magnifier
+    -- awful.layout.suit.magnifier
     -- awful.layout.suit.floating,
     -- awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
