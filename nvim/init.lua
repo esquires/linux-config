c19e9318ae2661ee60984e3bdba862557e3afff0
Eric Squires <eric.g.squires@gmail.com>
2024-11-08 06:42:38 -0500

    update terminal highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1159,3 +1159,26 @@ cmd('nnoremap <localleader>s :lua GoToDefinitionInNewTab()<cr>')
 
 vim.o.foldminlines = 5
 vim.o.foldnestmax = 2
+
+-- https://vi.stackexchange.com/a/34722
+-- vim.g.terminal_color_2 = '#fffcf5'
+-- vim.g.terminal_color_2 = '#373737'
+-- https://github.com/neovim/neovim/issues/2897#issuecomment-115464516
+-- vim.g.terminal_color_0  = '#2e3436'
+-- vim.g.terminal_color_1  = '#cc0000'
+vim.g.terminal_color_2  = '#4e9a06'
+vim.g.terminal_color_2  = '#C68539'
+vim.g.terminal_color_4  = '#397AC6'
+-- vim.g.terminal_color_3  = '#c4a000'
+-- vim.g.terminal_color_4  = '#3465a4'
+-- vim.g.terminal_color_5  = '#75507b'
+-- vim.g.terminal_color_6  = '#0b939b'
+-- vim.g.terminal_color_7  = '#d3d7cf'
+-- vim.g.terminal_color_8  = '#555753'
+-- vim.g.terminal_color_9  = '#ef2929'
+-- vim.g.terminal_color_10 = '#8ae234'
+-- vim.g.terminal_color_11 = '#fce94f'
+-- vim.g.terminal_color_12 = '#729fcf'
+-- vim.g.terminal_color_13 = '#ad7fa8'
+-- vim.g.terminal_color_14 = '#00f5e9'
+-- vim.g.terminal_color_15 = '#eeeeec'
